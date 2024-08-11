47dda91e35cd2e3831462c3f7bd3f258eb09abb5
John Wilger <john@johnwilger.com>
2024-08-10 19:51:08 -0700

    Add lvm kernel modules

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.initrd.kernelModules = [
+    "dm-snapshot"
+    "dm-raid"
+    "dm-cache-default"
+  ];
 
   networking.hostName = "gregor"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
