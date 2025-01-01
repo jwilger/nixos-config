352350af089e476683a3680c2fe85b28690c9a89
John Wilger <john@johnwilger.com>
2024-12-31 18:02:22 -0800

    Updated hardware config and removed traces of cosmic desktop

--- flake.nix
+++ flake.nix
@@ -3,21 +3,12 @@
 
   inputs = {
     hyprland.url = "github:hyprwm/Hyprland";
-    nixpkgs = {
-      url = "github:NixOs/nixpkgs/nixos-unstable";
-      follows = "nixos-cosmic/nixpkgs";
-    };
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
-    };
-    stylix = {
-      url = "github:danth/stylix";
-    };
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    stylix.url = "github:danth/stylix";
   };
 
   outputs = {
     nixpkgs,
-    nixos-cosmic,
     stylix,
     ...
   } @ inputs: {
@@ -28,11 +19,10 @@
         modules = [
 	  {
 	    nix.settings = {
-	      substituters = [ "https://cosmic.cachix.org/" "https://hyprland.cachix.org/" ];
-	      trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+	      substituters = [ "https://hyprland.cachix.org/" ];
+	      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
 	    };
 	  }
-	  nixos-cosmic.nixosModules.default
           stylix.nixosModules.stylix
           ./configuration.nix
         ];
