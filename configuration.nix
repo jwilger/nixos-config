347561e0d5e4aab451d20a47eb74d1262ec88d17
John Wilger <john@johnwilger.com>
2025-01-14 13:56:28 -0800

    Set up so that tap capslock is esc, hold is ctrl

--- configuration.nix
+++ configuration.nix
@@ -70,6 +70,22 @@ in {
   };
 
   services = {
+    interception-tools =
+    let
+      itools = pkgs.interception-tools;
+      itools-caps = pkgs.interception-tools-plugins.caps2esc;
+    in
+    {
+      enable = true;
+      plugins = [ itools-caps ];
+      udevmonConfig = pkgs.lib.mkDefault ''
+        - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc -m 0 | ${itools}/bin/uinput -d $DEVNODE"
+          DEVICE:
+            EVENTS:
+              EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+      '';
+    };
+    
     fstrim.enable = true;
 
     greetd = {
