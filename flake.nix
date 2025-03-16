82a247c02c301cb632fcf0d6371396581a585be7
John Wilger <john@johnwilger.com>
2025-03-16 13:25:32 -0700

    Update packages

--- flake.nix
+++ flake.nix
@@ -11,22 +11,22 @@
     nixpkgs,
     stylix,
     ...
-  } @ inputs: {
-    nixosConfigurations = {
-      gregor = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [
-	  {
-	    nix.settings = {
-	      substituters = [ "https://hyprland.cachix.org/" ];
-	      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-	    };
-	  }
-          stylix.nixosModules.stylix
-          ./configuration.nix
-        ];
+    } @ inputs: {
+      nixosConfigurations = {
+        gregor = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = [
+            {
+              nix.settings = {
+                substituters = [ "https://hyprland.cachix.org/" ];
+                trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+              };
+            }
+            stylix.nixosModules.stylix
+            ./configuration.nix
+          ];
+        };
       };
     };
-  };
 }
