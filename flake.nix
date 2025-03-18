11fa87dda5352fcc98fcefb8cc4b432177697037
John Wilger <john@johnwilger.com>
2025-03-18 06:37:41 -0700

    Numrous fixes to my environment

--- flake.nix
+++ flake.nix
@@ -2,29 +2,40 @@
   description = "NixOX System Configuration";
 
   inputs = {
-    hyprland.url = "github:hyprwm/Hyprland";
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
     stylix.url = "github:danth/stylix";
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     stylix,
+    home-manager,
     ...
-    } @ inputs: {
+    }: {
       nixosConfigurations = {
         gregor = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
           system = "x86_64-linux";
           modules = [
             {
-              nix.settings = {
-                substituters = [ "https://hyprland.cachix.org/" ];
-                trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-              };
+              nixpkgs.config.allowUnfree = true;
             }
             stylix.nixosModules.stylix
             ./configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = false;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+
+              home-manager.users.jwilger = import ./home.nix;
+            }
+            ({lib, ...}: {
+              nix.registry.nixpkgs.flake = nixpkgs;
+            })
           ];
         };
       };
