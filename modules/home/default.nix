a5aa1b90c074b91389e1093e511b3cc5bb5bebac
John Wilger <john@johnwilger.com>
2025-04-12 09:24:21 -0700

    Add insync to sync google drive to local filesystem

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,4 +1,4 @@
-{inputs, username, host, ...}: {
+{...}: {
   imports =
        [(import ./bat.nix)]                       # better cat command
     ++ [(import ./btop.nix)]                      # resouces monitor 
@@ -7,13 +7,14 @@
     ++ [(import ./git.nix)]                       # version control
     ++ [(import ./gtk.nix)]                       # gtk theme
     ++ [(import ./hyprland)]                      # window manager
+    ++ [(import ./insync)]                        # google drive client
     ++ [(import ./kitty.nix)]                     # terminal
     ++ [(import ./swaync/swaync.nix)]             # notification deamon
-    ++ [(import ./nvim)]                      # neovim editor
+    ++ [(import ./nvim)]                          # neovim editor
     ++ [(import ./packages.nix)]                  # other packages
     ++ [(import ./scripts/scripts.nix)]           # personal scripts
     ++ [(import ./starship.nix)]                  # shell prompt
     ++ [(import ./waybar)]                        # status bar
-    ++ [(import ./yazi)]                      # terminal-based file explorer
+    ++ [(import ./yazi)]                          # terminal-based file explorer
     ++ [(import ./zsh.nix)];                      # shell
 }
