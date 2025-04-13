9719744d8cfc96de982c78884099fdcf123e4383
John Wilger <john@johnwilger.com>
2025-04-12 23:19:54 -0700

    Allow local webserver to be started by normal user

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }: 
+{ host, pkgs, ... }: 
 {
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" =
+    if host == "gregor"
+      then 0
+    else null;
+
   networking = {
     networkmanager.enable = true;
     firewall = {
       enable = true;
+      allowedTCPPorts = [22 80 443];
     };
   };
 
