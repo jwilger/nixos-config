f3770aeab55189e7a8a122187aec7c53a7dca9bb
John Wilger <john@johnwilger.com>
2025-04-14 20:18:26 -0700

    Update completions configuration for neovim
    I hate surprise auto-completions.

---
+++ modules/home/nvim/nvim/lua/plugins/completions.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = { preset = "enter" },
+      appearance = { nerd_font_variant = "mono" },
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
+        },
+      },
+    },
+  },
+}
