42b98e80de5e9aba0b8cd72a6fba695b3bf4d98a
John Wilger <john@johnwilger.com>
2025-04-27 11:12:31 -0700

    sort hyprland vars alphabetically

--- modules/home/desktop/hyprland/variables.nix
+++ modules/home/desktop/hyprland/variables.nix
@@ -1,30 +1,29 @@
 { ... }:
 {
   home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    __GL_GSYNC_ALLOWED = "0";
-    __GL_VRR_ALLOWED = "0";
-    _JAVA_AWT_WM_NONEREPARENTING = "1";
+    ANKI_WAYLAND = "1";
+    CLUTTER_BACKEND = "wayland";
+    CURSOR_SIZE = "32";
+    CURSOR_THEME = "catppuccin-mocha-lavender-cursors";
+    DIRENV_LOG_FORMAT = "";
     DISABLE_QT5_COMPAT = "0";
     GDK_BACKEND = "wayland";
-    ANKI_WAYLAND = "1";
-    DIRENV_LOG_FORMAT = "";
-
+    MOZ_ENABLE_WAYLAND = "1";
+    NIXOS_OZONE_WL = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     QT_QPA_PLATFORM = "xcb";
     QT_QPA_PLATFORMTHEME = "qt5ct";
     QT_STYLE_OVERRIDE = "kvantum";
-    MOZ_ENABLE_WAYLAND = "1";
-    WLR_BACKEND = "drm";
-    WLR_RENDERER = "gles2";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    XDG_SESSION_TYPE = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     SDL_VIDEODRIVER = "wayland";
-    CLUTTER_BACKEND = "wayland";
-    CURSOR_THEME = "catppuccin-mocha-lavender-cursors";
-    CURSOR_SIZE = "32";
-    XCURSOR_THEME = "catppuccin-mocha-lavender-cursors";
+    WLR_BACKEND = "drm";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_RENDERER = "gles2";
     XCURSOR_SIZE = "32";
+    XCURSOR_THEME = "catppuccin-mocha-lavender-cursors";
+    XDG_SESSION_TYPE = "wayland";
+    _JAVA_AWT_WM_NONEREPARENTING = "1";
+    __GL_GSYNC_ALLOWED = "0";
+    __GL_VRR_ALLOWED = "0";
   };
 }
