71ec30bdebd62e7188160320f128e283e345bacb
John Wilger <john@johnwilger.com>
2025-10-13 10:54:34 -0700

    Updates to yazi config

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -1,7 +1,34 @@
-{...}:
+{ pkgs, ... }:
 {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+    plugins = with pkgs.yaziPlugins; {
+      git = git;
+      sudo = sudo;
+      glow = glow;
+      rsync = rsync;
+      piper = piper;
+      lazygit = lazygit;
+      starship = starship;
+      projects = projects;
+      mediainfo = mediainfo;
+      toggle-pane = toggle-pane;
+      time-travel = time-travel;
+    };
   };
+
+  xdg.configFile."yazi/yazi.toml".text = ''
+    [mgr]
+    ratio = [1, 3, 4]
+
+    [plugin]
+    prepend_previewers = [
+      { name = "*.md", run = "glow" },
+    ]
+
+    [preview]
+    tab_size = 2
+    wrap = "true"
+  '';
 }
