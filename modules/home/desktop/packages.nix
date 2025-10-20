6d0b4c8260f02523e5af2c549265a20c6c9f19a7
John Wilger <john@johnwilger.com>
2025-10-20 14:14:35 -0700

    Add caffeine applet to COSMIC panel for idle prevention control

--- modules/home/desktop/packages.nix
+++ modules/home/desktop/packages.nix
@@ -3,6 +3,7 @@
   home.packages = (
     with pkgs;
     [
+      cosmic-ext-applet-caffeine # caffeine applet for COSMIC desktop
       nixfmt-rfc-style
       pavucontrol # pulseaudio volume controle (GUI)
     ]
