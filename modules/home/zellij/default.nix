158f0a1f3759987b7901dd1218fbf34f41ce02f7
John Wilger <john@johnwilger.com>
2025-11-29 11:26:53 -0800

    adopt catppuccin zjstatus layout for zellij

--- modules/home/zellij/default.nix
+++ modules/home/zellij/default.nix
@@ -1,26 +1,16 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     zellij
   ];
 
   xdg.configFile."zellij/config.kdl".source = ./config.kdl;
-
-  # Provide a single, borderless pane while keeping Zellij's built-in status bar.
-  xdg.configFile."zellij/layouts/single-borderless.kdl".source =
-    pkgs.writeText "zellij-layout-single-borderless.kdl" ''
-      layout {
-        default_tab_template {
-          pane borderless=true
-        }
-      }
-    '';
-
+  xdg.configFile."zellij/layouts/default.kdl".source = ./layout.kdl;
   # Zellij-specific shell aliases
   programs.zsh.shellAliases = {
     # Attach or create session named after current directory
     zz = ''
-      ${pkgs.zellij}/bin/zellij --layout=.zellij.kdl attach -c "`basename \"$PWD\"`"
+      ${pkgs.zellij}/bin/zellij attach -c "`basename \"$PWD\"`"
     '';
 
     # Attach to first available session
