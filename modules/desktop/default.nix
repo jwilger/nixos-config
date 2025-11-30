32d000e3e758aabe40ac49021b930dbc2acfc0a7
John Wilger <john@johnwilger.com>
2025-11-30 10:39:44 -0800

    feat(desktop): remap keys for programming tasks
    Maps the left and right shift keys such that when they are tapped (not
    as part of a chord), they act as ( and ), respectively. Also tapping
    Capslock now acts as Escape while holding it acts as a Left Control key.
    
    Updated helix config with some keymappings that I am used to from vim.

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -32,9 +32,37 @@
   };
 
   services = {
+    interception-tools = {
+      enable = true;
+      plugins = [ pkgs.interception-tools-plugins.dual-function-keys ];
+      udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c /etc/dual-function-keys.yaml | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_RIGHTSHIFT, KEY_LEFTSHIFT]
+      '';
+    };
     displayManager.cosmic-greeter.enable = true;
     desktopManager.cosmic.enable = true;
   };
+
+  # For interception-tools config
+  environment.etc."dual-function-keys.yaml".text = ''
+  TIMING:
+    TAP_MILLISEC: 200
+    DOUBLE_TAP_MILLISEC: 0
+
+  MAPPINGS:
+    - KEY: KEY_LEFTSHIFT
+      TAP: [KEY_LEFTSHIFT, KEY_9]
+      HOLD: KEY_LEFTSHIFT
+    - KEY: KEY_RIGHTSHIFT
+      TAP: [KEY_RIGHTSHIFT, KEY_0]
+      HOLD: KEY_RIGHTSHIFT
+    - KEY: KEY_CAPSLOCK
+      TAP: KEY_ESC
+      HOLD: KEY_LEFTCTRL
+  '';
 
   environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 
