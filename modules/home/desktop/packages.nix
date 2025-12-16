c790301a9dbfb2ba585c22c83494e6ac572e426c
John Wilger <john@johnwilger.com>
2025-12-15 20:45:53 -0800

    update flake

--- modules/home/desktop/packages.nix
+++ modules/home/desktop/packages.nix
@@ -4,6 +4,7 @@
     with pkgs;
     [
       cosmic-ext-applet-caffeine # caffeine applet for COSMIC desktop
+      discord # chat for games
       nixfmt-rfc-style
       pavucontrol # pulseaudio volume controle (GUI)
       vscode-fhs # VS Code with FHS environment for extensions
