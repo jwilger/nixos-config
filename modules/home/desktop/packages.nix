39c50a81ab7fdf2b103c6a9537ffe7d2aa4e0415
John Wilger <john@johnwilger.com>
2026-01-05 09:39:20 -0800

    add kooha for screen recording

--- modules/home/desktop/packages.nix
+++ modules/home/desktop/packages.nix
@@ -5,6 +5,7 @@
     [
       cosmic-ext-applet-caffeine # caffeine applet for COSMIC desktop
       discord # chat for games
+      kooha # simple screen recorder (Wayland/Portal)
       nixfmt-rfc-style
       pavucontrol # pulseaudio volume controle (GUI)
     ]
