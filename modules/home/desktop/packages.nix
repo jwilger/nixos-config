34c5351fd4f7943a79f886beba46eec95f5d6958
John Wilger <john@johnwilger.com>
2026-01-15 08:27:02 -0800

    replace kooha with gpu-screen-recorder and remove nixfmt-rfc-style

--- modules/home/desktop/packages.nix
+++ modules/home/desktop/packages.nix
@@ -4,9 +4,8 @@
     with pkgs;
     [
       discord # chat for games
-      kooha # simple screen recorder (Wayland/Portal)
-      nixfmt-rfc-style
       pavucontrol # pulseaudio volume control (GUI)
+      gpu-screen-recorder
     ]
   );
 }
