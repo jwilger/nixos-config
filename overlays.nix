12da1bfd8ca0f0ee32f64e8ead5fca4ea869058e
John Wilger <john@johnwilger.com>
2026-01-26 13:45:02 -0800

    Fix polkit U2F helper sandbox

--- overlays.nix
+++ overlays.nix
@@ -1,20 +1,5 @@
+{ ... }:
 {
-  inputs,
-  pkgs,
-  ...
-}:
-let
-  # Import polkit 126 from pinned nixpkgs (127 breaks pam_u2f/YubiKey)
-  # See: https://github.com/polkit-org/polkit/issues/622
-  pkgs-polkit126 = import inputs.nixpkgs-polkit126 {
-    inherit (pkgs.stdenv.hostPlatform) system;
-    config.allowUnfree = true;
-  };
-in
-{
-  nixpkgs.overlays = [
-    (final: prev: {
-      polkit = pkgs-polkit126.polkit;
-    })
-  ];
+  # No overlays currently enabled.
+  nixpkgs.overlays = [ ];
 }
