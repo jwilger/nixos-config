675be7cb1baec68c6471c7d111cb2844f18a6b70
John Wilger <john@johnwilger.com>
2026-05-17 19:03:10 -0700

    sansa-vm config

--- hosts/sansa-vm/default.nix
+++ hosts/sansa-vm/default.nix
@@ -14,10 +14,20 @@
   # The generated x86 VM profile uses kvm-intel; this host is arm64.
   boot.kernelModules = lib.mkForce [ ];
 
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    device = "nodev";
+  };
+
+  boot.loader.efi = {
+    canTouchEfiVariables = lib.mkForce false;
+    efiSysMountPoint = "/boot";
+  };
+
   # qemu/UTM-style VMs commonly expose the disk as /dev/vda.
   boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = false;
 
   # Allow local remote access to make it easier to toy around with the system.
@@ -32,7 +42,7 @@
   users.users."${username}".initialPassword = "test";
 
   virtualisation.vmVariant = {
-    virtualisation.graphics = false;
+    virtualisation.graphics = true;
     services.timesyncd.enable = lib.mkForce true;
   };
 
